26610c23dd40e60740062bd49e3c730dd2af1ec9
gustavomauricio <gustavo.mauricio58@gmail.com>
2022-07-09 12:41:19 +0100

    fix: scripts loading strategy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,14 +35,14 @@ export default function Document() {
           src="scripts/jquery.min.js"
         ></Script>
         <Script
+          strategy="beforeInteractive"
+          src="scripts/smooth-scroll.min.js"
+        ></Script>
+        <Script
           strategy="lazyOnload"
           src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.19/jquery.touchSwipe.min.js"
         ></Script>
         <Script strategy="lazyOnload" src="scripts/bootstrap.min.js"></Script>
-        <Script
-          strategy="lazyOnload"
-          src="scripts/smooth-scroll.min.js"
-        ></Script>
         <Script strategy="lazyOnload" src="scripts/main.js"></Script>
       </body>
     </Html>
