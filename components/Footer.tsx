63cd268806d573ff62c8a4abfc03bf44f6925ea8
gustavomauricio <gustavo.mauricio58@gmail.com>
2022-07-14 12:28:25 +0100

    footer styling and content update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,29 +2,35 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-4 text-center">
-      <p className="font-semibold text-lg mb-2">@gustavommauricio</p>
-      <div className="w-full flex justify-center">
+    <footer className="py-6 text-center">
+      <div className="w-full flex justify-center gap-4">
+        <a
+          href="https://github.com/gustavomauricio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-xl fa-github color"></i>
+        </a>
         <a
           href="https://www.linkedin.com/in/gustavommauricio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-2x fa-linkedin color mx-2"></i>
+          <i className="fab fa-xl fa-linkedin color"></i>
         </a>
         <a
           href="https://twitter.com/gustavomwy"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-2x fa-twitter color mx-2"></i>
+          <i className="fab fa-xl fa-twitter color"></i>
         </a>
         <a
           href="https://instagram.com/gustavommauricio"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-2x fa-instagram color mx-2"></i>
+          <i className="fab fa-xl fa-instagram color"></i>
         </a>
       </div>
     </footer>
